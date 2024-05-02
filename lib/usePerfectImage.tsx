eb999fc45fa21515a2f85fbb3b6154fdb8515ad5
Abhishek Mamgain <abhishekmmgn@gmail.com>
2024-05-02 11:44:40 +0530

    Fix: image
    Signed-off-by: Abhishek Mamgain <abhishekmmgn@gmail.com>

--- lib/usePerfectImage.tsx
+++ lib/usePerfectImage.tsx
@@ -2,20 +2,22 @@
 
 import { useMediaQuery } from "usehooks-ts";
 
-type PropsType = {
-  poster_path: string;
-  backdrop_path: string;
-};
-
 export default function usePerfectImage(
   poster_path: string,
   backdrop_path: string
 ) {
-  const isDesktop = useMediaQuery("(min-width: 640px)");
-  const isLargeWidthDesktop = useMediaQuery("(min-width: 1280px)");
-  return isDesktop
+  const isDesktop = useMediaQuery("(min-width: 640px)", {
+    defaultValue: true,
+    initializeWithValue: false,
+  });
+  const isLargeWidthDesktop = useMediaQuery("(min-width: 1280px)", {
+    defaultValue: true,
+    initializeWithValue: false,
+  });
+  const image = isDesktop
     ? `https://image.tmdb.org/t/p/${
         isLargeWidthDesktop ? "original" : "w1280"
-      }${backdrop_path}`
-    : `https://image.tmdb.org/t/p/w780${poster_path}`;
+      }/${backdrop_path}`
+    : `https://image.tmdb.org/t/p/w780/${poster_path}`;
+  return image;
 }
